60f03e7ce5703aca24edc251173c61883ff7e50f
JehanSilva <it21804342@my.sliit.lk>
2024-05-08 17:15:46 +0530

    edited the nasa apod image date

--- nasa-react-app/src/components/Apod.jsx
+++ nasa-react-app/src/components/Apod.jsx
@@ -15,27 +15,38 @@ export default function Apod() {
     useEffect(() => {
         async function fetchAPIData() {
             const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
-            const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`
+            const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}` + '&date=2024-05-05'
 
-            const today = (new Date()).toDateString()
-            const localKey = `NASA-${today}`
-            if (localStorage.getItem(localKey)) {
-                const apiData = JSON.parse(localStorage.getItem(localKey))
-                setData(apiData)
-                console.log('fetched from the cache today')
-                return
-            }
-            localStorage.clear()
-
+            // new code start
             try {
-                const res = await fetch(url)
-                const apiData = await res.json()
-                localStorage.setItem(localKey, JSON.stringify(apiData))
-                setData(apiData)
-                console.log("fetched from API today")
+                const res = await fetch(url);
+                const apiData = await res.json();
+                setData(apiData);
+                console.log("fetched APOD image for May 6, 2024");
             } catch (err) {
-                console.log(err.message)
+                console.log(err.message);
             }
+            // new code end
+
+            // const today = (new Date()).toDateString()
+            // const localKey = `NASA-${today}`
+            // if (localStorage.getItem(localKey)) {
+            //     const apiData = JSON.parse(localStorage.getItem(localKey))
+            //     setData(apiData)
+            //     console.log('fetched from the cache today')
+            //     return
+            // }
+            // localStorage.clear()
+
+            // try {
+            //     const res = await fetch(url)
+            //     const apiData = await res.json()
+            //     localStorage.setItem(localKey, JSON.stringify(apiData))
+            //     setData(apiData)
+            //     console.log("fetched from API today")
+            // } catch (err) {
+            //     console.log(err.message)
+            // }
         }
         fetchAPIData()
     }, [])
